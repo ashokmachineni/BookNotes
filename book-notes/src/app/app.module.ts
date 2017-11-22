ed65df8d27a2a376aa82177684823fdf7b6c771a
ashokmachineni <ashok5679m@gmail.com>
2017-11-22 14:16:36 -0500

    Initial commit

---
+++ book-notes/src/app/app.module.ts
@@ -0,0 +1,19 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { HttpModule } from '@angular/http';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpModule
+  ],
+  providers: [],
+  bootstrap: [HomeComponent]
+})
+export class AppModule { }
---
+++ book-notes/src/app/home/home.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { Http } from '@angular/http';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+  posts: any[];
+
+
+    constructor(http: Http) {
+      http.get('https://jsonplaceholder.typicode.com/posts')
+      .subscribe(response => {
+        this.posts = response.json();
+      });
+   }
+
+  ngOnInit() {
+  }
+
+}
